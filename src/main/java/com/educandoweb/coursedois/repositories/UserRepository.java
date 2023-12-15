f7b072083ce8a1a79e5b2d08be69a927bde6b200
LuisVynicius <luismevy@gmail.com>
2023-12-15 11:35:34 -0300

    Raimundo parte 1

--- src/main/java/com/educandoweb/coursedois/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/coursedois/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.educandoweb.coursedois.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.coursedois.entities.User;
-
+//Não precisa por causa do extends
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
